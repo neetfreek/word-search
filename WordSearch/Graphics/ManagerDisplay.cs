1215a68396490226e46e6ea6284960e904704156
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-04-16 08:04:19 +0300

    Reorganise, clarify, ManagerDisplay.cs

--- WordSearch/Graphics/ManagerDisplay.cs
+++ WordSearch/Graphics/ManagerDisplay.cs
@@ -7,59 +7,59 @@ namespace WordSearch
     {
         public Matrix ScaleMatrix;
 
-        private int heightTarget;
         private int widthTarget;
-        private readonly int heightVirtual;
+        private int heightTarget;
         private readonly int widthVirtual;
+        private readonly int heightVirtual;
 
-        private readonly float scaleHeightScreen;
         private readonly float scaleWidthScreen;
+        private readonly float scaleHeightScreen;
 
-        public int HeightTargetScaled
-        {
-            get { return (int)(heightTarget * ScaleHeight); }
-        }
         public int WidthTargetScaled
         {
             get { return (int)(widthTarget * ScaleWidth); }
         }
-
-        public float ScaleHeight
+        public int HeightTargetScaled
         {
-            get { return scaleHeightScreen; }
+            get { return (int)(heightTarget * ScaleHeight); }
         }
+
         public float ScaleWidth
         {
             get { return scaleWidthScreen; }
+        }
+        public float ScaleHeight
+        {
+            get { return scaleHeightScreen; }
         }
 
         public ManagerDisplay(GraphicsDeviceManager gdManager)
         {
             // Setup preferred BackBuffer dimensions
-            gdManager.PreferredBackBufferHeight = 1080;
             gdManager.PreferredBackBufferWidth = 1920;
+            gdManager.PreferredBackBufferHeight = 1080;
 
             // Setup viewport
             gdManager.GraphicsDevice.PresentationParameters.BackBufferWidth = gdManager.PreferredBackBufferWidth;
             gdManager.GraphicsDevice.PresentationParameters.BackBufferHeight = gdManager.PreferredBackBufferHeight;
-            gdManager.GraphicsDevice.Viewport = new Viewport(0, 0, gdManager.PreferredBackBufferHeight, gdManager.PreferredBackBufferWidth);
+            gdManager.GraphicsDevice.Viewport = new Viewport(0, 0, gdManager.PreferredBackBufferWidth, gdManager.PreferredBackBufferHeight);
 
             // Set heights, widths
-            heightVirtual = 1080;
             widthVirtual = 1920;
-            heightTarget = gdManager.GraphicsDevice.DisplayMode.Height;
+            heightVirtual = 1080;
             widthTarget = gdManager.GraphicsDevice.DisplayMode.Width;
+            heightTarget = gdManager.GraphicsDevice.DisplayMode.Height;
 
             // Set scales
-            scaleHeightScreen = ((float)heightVirtual / (float)heightTarget);
             scaleWidthScreen = ((float)widthVirtual / (float)widthTarget);
-            float scaleHeightSprite = ((float)heightTarget / (float)heightVirtual);
+            scaleHeightScreen = ((float)heightVirtual / (float)heightTarget);
             float scaleWidthSprite = ((float)widthTarget / (float)widthVirtual);
+            float scaleHeightSprite = ((float)heightTarget / (float)heightVirtual);
 
-            // Reset viewport with new BackBuffer dimensions
+            // Reset, back buffer dimensions, viewport with new BackBuffer dimensions
             gdManager.PreferredBackBufferHeight = HeightTargetScaled;
             gdManager.PreferredBackBufferWidth = WidthTargetScaled;
-            gdManager.GraphicsDevice.Viewport = new Viewport(0, 0, gdManager.PreferredBackBufferHeight, gdManager.PreferredBackBufferWidth);         
+            gdManager.GraphicsDevice.Viewport = new Viewport(0, 0, gdManager.PreferredBackBufferWidth, gdManager.PreferredBackBufferHeight);         
             gdManager.ApplyChanges();
 
             // Set display to full screen
