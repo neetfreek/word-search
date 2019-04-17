64f1648ec65a5a664cbf80e8f6657d242a56a15c
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-04-17 14:45:33 +0300

    Update Utility.cs

--- WordSearch/Common/Utility.cs
+++ WordSearch/Common/Utility.cs
@@ -10,11 +10,11 @@ namespace WordSearch.Common
         // Names
         public const string nameHeadingWordsList = "List of Words:";
         public static string nameHeadingNameList = "";
-        public const string nameBackground = "backgroundMenu", nameButtonStart = "start", nameButtonBack = "back",
+        public const string nameBackground = "backgroundMenu", nameButtonStart = "start", nameButtonMenu = "menu",
             nameButtonCategory = "categories", nameButtonSize = "sizes", nameButtonQuit = "quit",
             nameButtonSmall = "small", nameButtonMedium = "medium", nameButtonLarge = "large";
         // Text 
-        public const string textButtonStart = "Start Game", textButtonBack = "Back",
+        public const string textButtonStart = "Start Game", textbuttonMenu = "Menu",
             textButtonCategory = "Select a category", textButtonSize = "Select a Size:", textButtonQuit = "Quit Game",
             textButtonSmall = "6 Words", textButtonMedium = "10 Words", textButtonLarge = "16 Words";
     }
@@ -24,9 +24,19 @@ public enum ButtonMenu
 {
     none,
     start,
-    back,
+    menu,
     quit,
+    instruments,
+    mammals,
+    occupations,
     small,
     medium,
     large,
+}
+
+public enum SelectedMenu
+{
+    start,
+    categories,
+    sizes,
 }
