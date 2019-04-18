d97628a348c355e11b90f7aa7a02de15f043b611
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-04-18 12:48:16 +0300

    Fix tile adjacency test

--- WordSearch/ManagerSelectTile.cs
+++ WordSearch/ManagerSelectTile.cs
@@ -14,17 +14,19 @@ namespace WordSearch
 
             // If > 0 tiles in listTilesTemporary
             if (!MainGame.listTilesTemporary.Contains(MainGame.ClickedTile) &&
-                MainGame.listTilesTemporary.Count > 0)
+                MainGame.listTilesTemporary.Count > 0 && TileAdjacentToAdded())
             {
                 // Check clicked tile adjacent to any in listTilesTemporary
-                if (TileAdjacentToAdded())
+                if (MainGame.listTilesTemporary.Count > 1)
                 {
-                    AddTileListTilesTemporary();
+                    // Check same directionm
                 }
+                AddTileListTilesTemporary();
+
                 // Add clicked tile to listTilesTemporary
             }
             // Else add tile to listTilesTemporary
-            else
+            else if (MainGame.listTilesTemporary.Count == 0)
             {
                 AddTileListTilesTemporary();
             }
@@ -55,6 +57,7 @@ namespace WordSearch
 
         private static bool TileAdjacentToAdded()
         {
+            ButtonTile tileCompare = MainGame.listTilesTemporary[ MainGame.listTilesTemporary.Count - 1];
             Vector2 tilePosMidSelected;
             Vector2 tilePosMidCompare;
             Vector2 distanceVector;
@@ -64,20 +67,18 @@ namespace WordSearch
             tilePosMidSelected.X = MainGame.ClickedTile.Pos.X + (float)(MainGame.spriteLetters.WidthSprite * 0.5);
             tilePosMidSelected.Y = MainGame.ClickedTile.Pos.Y + (float)(MainGame.spriteLetters.HeightSprite * 0.5);
 
-            foreach (ButtonTile tile in MainGame.listTilesTemporary)
+            // if position within one tile of clicked tiles's position
+            // get center pos of clicked tile
+            tilePosMidCompare.X = tileCompare.Pos.X + (float)(MainGame.spriteLetters.WidthSprite * 0.5);
+            tilePosMidCompare.Y = tileCompare.Pos.Y + (float)(MainGame.spriteLetters.HeightSprite * 0.5);
+            // get distance between tiles
+            distanceVector = tilePosMidSelected - tilePosMidCompare;
+            distance = Vector2.Distance(tilePosMidSelected, tilePosMidCompare);
+            // if tiles close enough return
+            System.Console.WriteLine(distance);
+            if (distance < sizeTile * 2)
             {
-                // if position within one tile of clicked tiles's position
-                // get center pos of clicked tile
-                tilePosMidCompare.X = tile.Pos.X + (float)(MainGame.spriteLetters.WidthSprite * 0.5);
-                tilePosMidCompare.Y = tile.Pos.Y + (float)(MainGame.spriteLetters.HeightSprite * 0.5);
-                // get distance between tiles
-                distanceVector = tilePosMidSelected - tilePosMidCompare;
-                distance = Vector2.Distance(tilePosMidSelected, tilePosMidCompare);
-                // if tiles close enough return
-                if (distance < sizeTile * 2)
-                {
-                    return true;
-                }
+                return true;
             }
 
             return false;
