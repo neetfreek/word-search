2bf2b244f7aabe1f1e774b23c60390cdf812e810
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-04-18 14:29:53 +0300

    Add same bearing restriction to tile select

--- WordSearch/ManagerSelectTile.cs
+++ WordSearch/ManagerSelectTile.cs
@@ -8,35 +8,50 @@ namespace WordSearch
 {
     public static class ManagerSelectTile
     {
+        private static float distance;
+        private static float distanceOld;
+        private static bool distanceOldSet;
+
         public static void SelectTile()
         {
             MainGame.ClickedTile = MainGame.MousedOverTile;
 
             // If > 0 tiles in listTilesTemporary
             if (!MainGame.listTilesTemporary.Contains(MainGame.ClickedTile) &&
-                MainGame.listTilesTemporary.Count > 0 && TileAdjacentToAdded())
+                MainGame.listTilesTemporary.Count > 0  && TileAdjacentToAdded())
             {
-                // Check clicked tile adjacent to any in listTilesTemporary
+                // Third+ tiles
                 if (MainGame.listTilesTemporary.Count > 1)
                 {
-                    // Check same directionm
+                    if (SameBearing())
+                    {
+                        AddTileListTilesTemporary();
+                    }
                 }
-                AddTileListTilesTemporary();
-
-                // Add clicked tile to listTilesTemporary
+                // Second tile
+                else if (MainGame.listTilesTemporary.Count == 1)
+                {
+                    AddTileListTilesTemporary();
+                }
             }
-            // Else add tile to listTilesTemporary
+            // First tile
             else if (MainGame.listTilesTemporary.Count == 0)
             {
                 AddTileListTilesTemporary();
             }
-
         }
 
         public static void UnselectTile()
         {
             MainGame.ClickedTile = MainGame.MousedOverTile;
             RemoveTileListTilesTemporary();
+        }
+
+        public static void ResetValues()
+        {
+            distance = 0;
+            distanceOld = 0;
+            distanceOldSet = false;
         }
 
         private static void AddTileListTilesTemporary()
@@ -52,6 +67,11 @@ namespace WordSearch
             if (MainGame.listTilesTemporary.Contains((MainGame.ClickedTile)))
             {
                 MainGame.listTilesTemporary.Remove(MainGame.ClickedTile);
+
+                if (MainGame.listTilesTemporary.Count == 0)
+                {
+                    ResetValues();
+                }
             }
         }
 
@@ -61,22 +81,38 @@ namespace WordSearch
             Vector2 tilePosMidSelected;
             Vector2 tilePosMidCompare;
             Vector2 distanceVector;
-            float distance;
+            //float distance;
             float sizeTile = (MainGame.spriteLetters.WidthSprite + MainGame.spriteLetters.HeightSprite) * 0.5f;
 
             tilePosMidSelected.X = MainGame.ClickedTile.Pos.X + (float)(MainGame.spriteLetters.WidthSprite * 0.5);
             tilePosMidSelected.Y = MainGame.ClickedTile.Pos.Y + (float)(MainGame.spriteLetters.HeightSprite * 0.5);
 
-            // if position within one tile of clicked tiles's position
             // get center pos of clicked tile
             tilePosMidCompare.X = tileCompare.Pos.X + (float)(MainGame.spriteLetters.WidthSprite * 0.5);
             tilePosMidCompare.Y = tileCompare.Pos.Y + (float)(MainGame.spriteLetters.HeightSprite * 0.5);
             // get distance between tiles
             distanceVector = tilePosMidSelected - tilePosMidCompare;
             distance = Vector2.Distance(tilePosMidSelected, tilePosMidCompare);
+            if (!distanceOldSet)
+            {
+                distanceOld = distance;
+                Console.WriteLine("DISTANCE OLD SET");
+                distanceOldSet = true;
+            }
             // if tiles close enough return
-            System.Console.WriteLine(distance);
             if (distance < sizeTile * 2)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // Check if next tile chosen is along the same bearing as previous selections
+        private static bool SameBearing()
+        {
+            Console.WriteLine($"distance: {distance}, distanceOld: {distanceOld}");
+
+            if (distance == distanceOld)
             {
                 return true;
             }
